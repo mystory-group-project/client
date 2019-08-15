d5218abafeed4db7911326aa30a14e8225fb7ab0
tyogautomo <yoga.utomo27@gmail.com>
2019-08-15 15:57:07 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <LandingPage></LandingPage>
+  </div>
+</template>
+
+<script>
+import LandingPage from "./components/LandingPage";
+
+export default {
+  components: {
+    LandingPage
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <FormLogin></FormLogin>
+</template>
+
+<script>
+import FormLogin from "./FormLogin.vue";
+
+export default {
+  components: {
+    FormLogin
+  }
+};
+</script>
+
+<style>
+</style>
